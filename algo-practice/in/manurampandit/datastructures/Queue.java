49902c0dc7848065176f60cababb2cd9c250ed2f
manu ram pandit <manupandit123@gmail.com>
2013-07-20 20:35:40 +0530

    Queue class changes:Added iterator

--- algo-practice/in/manurampandit/datastructures/Queue.java
+++ algo-practice/in/manurampandit/datastructures/Queue.java
@@ -3,86 +3,95 @@
  */
 package in.manurampandit.datastructures;
 
+import java.util.Iterator;
 
 /**
- * @author manurampandit
- * TODO: handle overflow and underflow scenario 
+ * @author manurampandit 
  */
-public class Queue<T> {
-	private Node<T> first, last;
+public class Queue<T> implements Iterable<T> {
+	private Node first, last;
+	private int size;
 
-	/**
-	 * @author manurampandit
+	/*
 	 * Inner class containing Node
+	 * 
 	 * @param <T>
 	 */
-	private class Node<T> {
+	private class Node {
 		T item;
-		Node<T> next;
+		Node next;
 
 		public Node(T item) {
 			this.item = item;
 			this.next = null;
 		}
 	}
-	
+
 	/**
-	 *  if queue is empty, it returns true else false
+	 * If queue is empty, it returns true else false
+	 * 
 	 * @return
 	 */
 	public boolean isEmpty() {
 		return first == null;
 	}
-	
+
 	/**
 	 * Enqueue an item in the queue.
+	 * 
 	 * @param item
 	 */
 	public void enqueue(T item) {
-		Node<T> oldLast=last;
-		last= new Node<T>(item);
+		Node oldLast = last;
+		last = new Node(item);
 		/*
-		 * assign first to last if queue is empty
-		 *  else assign reference of last node to oldNode
+		 * assign first to last if queue is empty else assign reference of last
+		 * node to oldNode
 		 */
-		if(isEmpty()){
-			first=last;
+		if (isEmpty()) {
+			first = last;
+		} else {
+			oldLast.next = last;
 		}
-		else{
-			oldLast.next=last;
-		}
+		this.size++;
 	}
+
 	/**
+	 * Dequeue the first item
 	 * 
-	 * @return
+	 * @return the first item as well
 	 */
 	public T dequeue() {
+		if (first == null)
+			return null;
 		T firstTemp = first.item;
 		first = first.next;
-		if(isEmpty())
-			last=null;
+		if (isEmpty())
+			last = null;
+		this.size--;
 		return firstTemp;
 	}
+
 	/**
 	 * print the current queue
 	 */
-	public void printQueue(){
-		Node<T> firstCopy=first;
+	public void printQueue() {
+		Node firstCopy = first;
 		System.out.print("Queue Elements:");
-		while(firstCopy!=null){
+		while (firstCopy != null) {
 			System.out.print(firstCopy.item + ",");
-			firstCopy=firstCopy.next;
+			firstCopy = firstCopy.next;
 		}
 		System.out.println();
 	}
-	
+
 	/**
 	 * @param args
-	 * Test client for enqueue and dequeue operation on queue ds
+	 *            Test client for enqueue and dequeue operation on queue ds
 	 */
 	public static void main(String[] args) {
-		Queue<Integer> queue= new Queue<>();
-		//enqueue some item
+		Queue<Integer> queue = new Queue<>();
+		// enqueue some item
 		queue.enqueue(4);
 		queue.printQueue();
 		queue.enqueue(5);
@@ -92,8 +101,64 @@ public class Queue<T> {
 		queue.dequeue();
 		queue.printQueue();
 		queue.dequeue();
-		queue.printQueue();
-		
+		queue.enqueue(33);
+		queue.enqueue(22);
+		System.out.println("Dequeue element:" + queue.dequeue());
+		System.out
+				.println("*********printing stack element using Iterator***************");
+		for (Integer qElement : queue) {
+			System.out.print(qElement + "\t");
+		}
+
 	}
 
+	@Override
+	/**
+	 * return queue iterator in FIFO order  
+	 */
+	public Iterator<T> iterator() {
+		return new QueueIterator();
+	}
+
+	/**
+	 * @return the size
+	 */
+	public int getSize() {
+		return size;
+	}
+
+	/*
+	 * CUSTOM queue iterator
+	 */
+	private class QueueIterator implements Iterator<T> {
+
+		Node firstCopy = first;
+
+		@Override
+		/**
+		 * checks if queue has next element
+		 */
+		public boolean hasNext() {
+			return firstCopy != null;
+		}
+
+		@Override
+		/**
+		 * return the next element
+		 */
+		public T next() {
+			T item = firstCopy.item;
+			firstCopy = firstCopy.next;
+			return item;
+		}
+
+		@Override
+		/**
+		 * method not implemented
+		 */
+		public void remove() {
+			// NOt implemented
+		}
+
+	}
 }
